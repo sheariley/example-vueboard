be68355b5e311e05eeb7091b042e9c7e1ee444bb
Shea Riley <sheariley@live.com>
2025-10-01 08:06:16 -0500

    Initial project setup

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@nuxt/test-utils',
+    '@nuxt/scripts',
+    '@pinia/nuxt',
+    // '@nuxtjs/supabase'
+  ]
+})
