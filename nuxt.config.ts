d748535a7cbee769f3b48ea7dbdd6fc0eed59d7e
Shea Riley <sheariley@live.com>
2025-10-07 06:50:04 -0500

    Enable debugging in Nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  debug: true,
+  
   devtools: {
     enabled: true,
     vscode: {}
