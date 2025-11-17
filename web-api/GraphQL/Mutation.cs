febfc3b5f5e8bbc12d6ecf7ca3d11e073a48d401
Shea Riley <sheariley@live.com>
2025-11-17 14:58:37 -0600

    Fix formatting of Mutation.cs code

--- web-api/GraphQL/Mutation.cs
+++ web-api/GraphQL/Mutation.cs
@@ -6,42 +6,42 @@ using Vueboard.DataAccess.Repositories;
 
 namespace Vueboard.Api.GraphQL
 {
-    public class Mutation
+  public class Mutation
+  {
+    private readonly IProjectRepository _projectRepo;
+    public Mutation(IProjectRepository projectRepo)
     {
-        private readonly IProjectRepository _projectRepo;
-        public Mutation(IProjectRepository projectRepo)
-        {
-            _projectRepo = projectRepo;
-        }
-
-        public Project CreateProject(CreateProjectInput input)
-        {
-            var project = new Project
-            {
-                Title = input.Title,
-                Description = input.Description,
-                DefaultCardFgColor = input.DefaultCardFgColor,
-                DefaultCardBgColor = input.DefaultCardBgColor,
-                Created = DateTime.UtcNow,
-                Updated = DateTime.UtcNow,
-                IsDeleted = false,
-                Uid = Guid.NewGuid(),
-                UserId = Guid.NewGuid(), // Replace with actual user context
-                Columns = new List<ProjectColumn>()
-            };
-            return _projectRepo.Add(project);
-        }
-
-        public Project? UpdateProject(Project updatedProject)
-        {
-            updatedProject.Updated = DateTime.UtcNow;
-            var success = _projectRepo.Update(updatedProject);
-            return success ? updatedProject : null;
-        }
-
-        public bool DeleteProject(string uid)
-        {
-            return _projectRepo.Delete(uid);
-        }
+      _projectRepo = projectRepo;
     }
+
+    public Project CreateProject(CreateProjectInput input)
+    {
+      var project = new Project
+      {
+        Title = input.Title,
+        Description = input.Description,
+        DefaultCardFgColor = input.DefaultCardFgColor,
+        DefaultCardBgColor = input.DefaultCardBgColor,
+        Created = DateTime.UtcNow,
+        Updated = DateTime.UtcNow,
+        IsDeleted = false,
+        Uid = Guid.NewGuid(),
+        UserId = Guid.NewGuid(), // Replace with actual user context
+        Columns = new List<ProjectColumn>()
+      };
+      return _projectRepo.Add(project);
+    }
+
+    public Project? UpdateProject(Project updatedProject)
+    {
+      updatedProject.Updated = DateTime.UtcNow;
+      var success = _projectRepo.Update(updatedProject);
+      return success ? updatedProject : null;
+    }
+
+    public bool DeleteProject(string uid)
+    {
+      return _projectRepo.Delete(uid);
+    }
+  }
 }
