5c73f80acd264f8433f0432965ab5fb4ef05c53e
Shea Riley <sheariley@live.com>
2025-11-17 14:59:07 -0600

    Fix formatting of Types.cs code

--- web-api/GraphQL/Types.cs
+++ web-api/GraphQL/Types.cs
@@ -4,60 +4,60 @@ using Vueboard.DataAccess.Models;
 
 namespace Vueboard.Api.GraphQL
 {
-    public class ProjectType : ObjectType<Project>
+  public class ProjectType : ObjectType<Project>
+  {
+    protected override void Configure(IObjectTypeDescriptor<Project> descriptor)
     {
-        protected override void Configure(IObjectTypeDescriptor<Project> descriptor)
-        {
-            descriptor.Field(f => f.Id).Type<NonNullType<IntType>>();
-            descriptor.Field(f => f.Uid).Type<NonNullType<StringType>>();
-            descriptor.Field(f => f.Created).Type<NonNullType<DateTimeType>>();
-            descriptor.Field(f => f.Updated).Type<NonNullType<DateTimeType>>();
-            descriptor.Field(f => f.IsDeleted).Type<NonNullType<BooleanType>>();
-            descriptor.Field(f => f.UserId).Type<NonNullType<StringType>>();
-            descriptor.Field(f => f.Title).Type<NonNullType<StringType>>();
-            descriptor.Field(f => f.Description).Type<StringType>();
-            descriptor.Field(f => f.DefaultCardFgColor).Type<StringType>();
-            descriptor.Field(f => f.DefaultCardBgColor).Type<StringType>();
-            descriptor.Field(f => f.Columns).Type<ListType<ProjectColumnType>>();
-        }
+      descriptor.Field(f => f.Id).Type<NonNullType<IntType>>();
+      descriptor.Field(f => f.Uid).Type<NonNullType<StringType>>();
+      descriptor.Field(f => f.Created).Type<NonNullType<DateTimeType>>();
+      descriptor.Field(f => f.Updated).Type<NonNullType<DateTimeType>>();
+      descriptor.Field(f => f.IsDeleted).Type<NonNullType<BooleanType>>();
+      descriptor.Field(f => f.UserId).Type<NonNullType<StringType>>();
+      descriptor.Field(f => f.Title).Type<NonNullType<StringType>>();
+      descriptor.Field(f => f.Description).Type<StringType>();
+      descriptor.Field(f => f.DefaultCardFgColor).Type<StringType>();
+      descriptor.Field(f => f.DefaultCardBgColor).Type<StringType>();
+      descriptor.Field(f => f.Columns).Type<ListType<ProjectColumnType>>();
     }
+  }
 
-    public class ProjectColumnType : ObjectType<ProjectColumn>
+  public class ProjectColumnType : ObjectType<ProjectColumn>
+  {
+    protected override void Configure(IObjectTypeDescriptor<ProjectColumn> descriptor)
     {
-        protected override void Configure(IObjectTypeDescriptor<ProjectColumn> descriptor)
-        {
-            descriptor.Field(f => f.Id).Type<NonNullType<IntType>>();
-            descriptor.Field(f => f.Uid).Type<NonNullType<StringType>>();
-            descriptor.Field(f => f.Created).Type<NonNullType<DateTimeType>>();
-            descriptor.Field(f => f.Updated).Type<NonNullType<DateTimeType>>();
-            descriptor.Field(f => f.IsDeleted).Type<NonNullType<BooleanType>>();
-            descriptor.Field(f => f.ProjectId).Type<NonNullType<IntType>>();
-            descriptor.Field(f => f.Name).Type<NonNullType<StringType>>();
-            descriptor.Field(f => f.IsDefault).Type<NonNullType<BooleanType>>();
-            descriptor.Field(f => f.FgColor).Type<StringType>();
-            descriptor.Field(f => f.BgColor).Type<StringType>();
-            descriptor.Field(f => f.Index).Type<NonNullType<IntType>>();
-            descriptor.Field(f => f.WorkItems).Type<ListType<WorkItemType>>();
-        }
+      descriptor.Field(f => f.Id).Type<NonNullType<IntType>>();
+      descriptor.Field(f => f.Uid).Type<NonNullType<StringType>>();
+      descriptor.Field(f => f.Created).Type<NonNullType<DateTimeType>>();
+      descriptor.Field(f => f.Updated).Type<NonNullType<DateTimeType>>();
+      descriptor.Field(f => f.IsDeleted).Type<NonNullType<BooleanType>>();
+      descriptor.Field(f => f.ProjectId).Type<NonNullType<IntType>>();
+      descriptor.Field(f => f.Name).Type<NonNullType<StringType>>();
+      descriptor.Field(f => f.IsDefault).Type<NonNullType<BooleanType>>();
+      descriptor.Field(f => f.FgColor).Type<StringType>();
+      descriptor.Field(f => f.BgColor).Type<StringType>();
+      descriptor.Field(f => f.Index).Type<NonNullType<IntType>>();
+      descriptor.Field(f => f.WorkItems).Type<ListType<WorkItemType>>();
     }
+  }
 
-    public class WorkItemType : ObjectType<WorkItem>
+  public class WorkItemType : ObjectType<WorkItem>
+  {
+    protected override void Configure(IObjectTypeDescriptor<WorkItem> descriptor)
     {
-        protected override void Configure(IObjectTypeDescriptor<WorkItem> descriptor)
-        {
-            descriptor.Field(f => f.Id).Type<NonNullType<IntType>>();
-            descriptor.Field(f => f.Uid).Type<NonNullType<StringType>>();
-            descriptor.Field(f => f.Created).Type<NonNullType<DateTimeType>>();
-            descriptor.Field(f => f.Updated).Type<NonNullType<DateTimeType>>();
-            descriptor.Field(f => f.IsDeleted).Type<NonNullType<BooleanType>>();
-            descriptor.Field(f => f.ProjectColumnId).Type<NonNullType<IntType>>();
-            descriptor.Field(f => f.Title).Type<NonNullType<StringType>>();
-            descriptor.Field(f => f.Description).Type<StringType>();
-            descriptor.Field(f => f.Notes).Type<StringType>();
-            descriptor.Field(f => f.FgColor).Type<StringType>();
-            descriptor.Field(f => f.BgColor).Type<StringType>();
-            descriptor.Field(f => f.Index).Type<NonNullType<IntType>>();
-            descriptor.Field(f => f.Tags).Type<ListType<NonNullType<StringType>>>(); // Expose tags as string array
-        }
+      descriptor.Field(f => f.Id).Type<NonNullType<IntType>>();
+      descriptor.Field(f => f.Uid).Type<NonNullType<StringType>>();
+      descriptor.Field(f => f.Created).Type<NonNullType<DateTimeType>>();
+      descriptor.Field(f => f.Updated).Type<NonNullType<DateTimeType>>();
+      descriptor.Field(f => f.IsDeleted).Type<NonNullType<BooleanType>>();
+      descriptor.Field(f => f.ProjectColumnId).Type<NonNullType<IntType>>();
+      descriptor.Field(f => f.Title).Type<NonNullType<StringType>>();
+      descriptor.Field(f => f.Description).Type<StringType>();
+      descriptor.Field(f => f.Notes).Type<StringType>();
+      descriptor.Field(f => f.FgColor).Type<StringType>();
+      descriptor.Field(f => f.BgColor).Type<StringType>();
+      descriptor.Field(f => f.Index).Type<NonNullType<IntType>>();
+      descriptor.Field(f => f.Tags).Type<ListType<NonNullType<StringType>>>(); // Expose tags as string array
     }
+  }
 }
