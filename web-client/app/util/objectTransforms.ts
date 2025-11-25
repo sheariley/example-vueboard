9dbad678e583cfe6a36df27b431a96673037b575
Shea Riley <sheariley@live.com>
2025-11-25 08:34:51 -0600

    Remove __typename prop in GraphQL response entity to writeable entity transform

--- web-client/app/util/objectTransforms.ts
+++ web-client/app/util/objectTransforms.ts
@@ -1,3 +1,5 @@
 export function createWriteableEntity<T extends object>(entity: T) {
-  return JSON.parse(JSON.stringify(entity));
+  const result = JSON.parse(JSON.stringify(entity));
+  if (typeof result.__typename !== 'undefined') delete result.__typename;
+  return result as T;
 }
